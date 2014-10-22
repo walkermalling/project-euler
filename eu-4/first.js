9aee56687cf3b25694ad9b194df974e3ed4052df
Walker Malling <walker.malling@gmail.com>
2014-10-22 16:31:27 -0700

    add solution

---
+++ eu-4/first.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var solution = function(){
+
+  // test if a number is a palindrome
+
+  this.isPalindrome = function(num){
+    var store = [];
+    while(num > 0){
+      store.push(num % 10);
+      num = Math.floor(num / 10);
+    }
+    while(store.length > 1){
+      if(store.shift() !== store.pop()) return false;
+    }
+ 
+    return true;
+  }; 
+
+  // iterate down from products of 3 digit numbers
+
+
+  this.findLargestPalindrome = function(digits){
+    var start = Math.pow(10,digits) - 1;
+    var store = [];
+
+    for(var k = start; k > 0; k--){
+      for(var m = start; m > 0; m--){
+        if(this.isPalindrome(k*m)){
+          store.push(k*m);
+          break;
+        }
+      }
+    }
+    
+    var max = store.reduce(function(a,b){
+      if(a > b) return a;
+      else return b;
+    }); 
+    return max;
+  };
+
+  
+};
+
+module.exports = solution;
