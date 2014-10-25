875bc6a2ad43bdf2942e37381b13475d02779cf2
Walker Malling <walker.malling@gmail.com>
2014-10-25 00:51:00 -0700

    add solution to #11

--- eu-11/first-test.js
+++ eu-11/first-test.js
@@ -18,7 +18,40 @@ describe('First Solution', function(){
     expect(solution).to.be.ok;
   });
 
-  it('...');
+  it('can read data from file',function(){
+    expect(solution.data).to.be.ok;
+    expect(typeof solution.data).to.eql('object'); 
+  });
+
+  it('can create a 20x20 array from filedata',function(){
+    expect(solution.data.length).to.eql(20);
+    solution.data.forEach(function(i){
+      expect(i.length).to.eql(20);
+    });
+  });
+
+  it('can get the horizontal product',function(){
+    expect(solution.getHorizontal(0,0)).to.be.ok;
+    expect(solution.getHorizontal(0,0)).to.eql(34144);
+  });
+
+  it('can get the vertical product',function(){
+    expect(solution.getVertical(0,0)).to.be.ok;
+    expect(solution.getVertical(0,0)).to.eql(1651104);
+  });
+  
+  it('can get the backward diagonal product',function(){
+    expect(solution.getBackwardDiagonal(0,0)).to.be.ok;
+  });
+
+  it('can get the forward diagonal product',function(){
+    expect(solution.getForwardDiagonal(0,0)).to.be.ok;
+  });
+
+  it('can get largest adjacency of the grid',function(){
+    expect(solution.findMax()).to.be.ok;
+  });
+  
 
 });
 
--- eu-11/first.js
+++ eu-11/first.js
@@ -1,2 +1,111 @@
 'use strict';
 /* First attempt at problem 11 */
+
+var fs = require('fs');
+var rootPath = require('app-root-path');
+
+var Solution = function(){
+  this.data = this.parseFile(); 
+  // this.largestAdjacency = this.findMax(); 
+};
+
+Solution.prototype.findMax = function(){
+  var prev = 0;
+  var current;
+  for(var k = 0; k < 20; k++){
+    for(var j = 0; j < 20; j++){
+      current = this.getAdjacency(k,j);
+      if(current > prev) prev = current;
+    }
+  } 
+  console.log(prev);
+  return prev;
+};
+
+Solution.prototype.getAdjacency = function(x,y){
+  var adjacencies = [
+    this.getHorizontal(x,y),
+    this.getVertical(x,y),
+    this.getForwardDiagonal(x,y),
+    this.getBackwardDiagonal(x,y)
+  ];
+  return adjacencies.reduce(function(a,b){
+    return a > b ? a : b;
+  });
+};
+
+Solution.prototype.getCell = function(x,y){
+  if( x < 0 || y < 0 || x > 19 || y > 19 ){
+    return 1; // equivalent of no value
+  } else {
+    return this.data[x][y];
+  }
+};
+
+Solution.prototype.getForwardDiagonal = function(x,y){
+  var row = x;
+  var column = y;
+  var store = [];
+  for(var k = 0; k < 4; k++){
+    store.push(this.getCell(row-k, column+k));
+  }
+  return store.reduce(function(a,b){
+    return a * b;
+  }); 
+};
+
+Solution.prototype.getBackwardDiagonal = function(x,y){
+  var row = x;
+  var column = y;
+  var store = [];
+  for(var k = 0; k < 4; k++){
+    store.push(this.getCell(row-k, column-k));
+  }
+  return store.reduce(function(a,b){
+    return a * b;
+  }); 
+}
+
+Solution.prototype.getHorizontal = function(x,y){
+  var row = x;
+  var column = y;
+  var store = []; 
+  for(var k = 0; k < 4; k++){
+    store.push(this.getCell(row, column+k));
+  }
+  return store.reduce(function(a,b){
+    return a * b;
+  });
+
+};
+
+Solution.prototype.getVertical = function(x,y){
+  var row = x;
+  var column = y;
+  var store = []; 
+  for(var k = 0; k < 4; k++){
+    store.push(this.getCell(row + k, column));
+  }
+  return store.reduce(function(a,b){
+    return a * b;
+  });
+
+};
+
+Solution.prototype.parseFile = function(){
+  var file = fs.readFileSync(rootPath + '/eu-11/data.txt').toString();
+  var arr = file.split(' ');
+  var x = [];
+  
+  var column = -1;
+  for(var k = 0; k < arr.length; k++){
+    if(k % 20 === 0){
+      column++;
+      x[column] = [];
+    }
+    x[column].push(parseInt(arr[k]));   
+  };
+  return x;
+};
+
+module.exports = Solution;
