c3513d96adf0d66af0bf131ed35fc129fb795593
Walker Malling <walker.malling@gmail.com>
2014-10-26 23:19:51 -0700

    make sure test dir exists

--- lib/seed.sh
+++ lib/seed.sh
@@ -53,6 +53,7 @@ describe('First Solution', function(){
 
 echo "linking test file into automocha..."
 
+mkdir -p test/automocha
 rm test/automocha/first-test.js
 ln eu-${number}/first-test.js test/automocha/
 
